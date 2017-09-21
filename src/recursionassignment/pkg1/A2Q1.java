17c86045bb26c95040c66dad4d27d66692c0470e
ratid6445 <ratid6445@HRH-IC0027347.wrdsb.ca>
2017-09-21 10:14:00 -0400

    Review - finished up to question 5

--- src/recursionassignment/pkg1/A2Q1.java
+++ src/recursionassignment/pkg1/A2Q1.java
@@ -17,45 +17,103 @@ public class A2Q1 {
         if (n >= 0 && n <= 9) {
             // return the single digit
             return n;
-        } 
-            // if the number is not a single digit 
-            // create a mod integer to yield the rightmost digit
-            int mod = n % 10;
-            // create a divide integer to remove the rightmost digit
-            int divide = n / 10;
-            // create a sum integer that goes through mod and divide 
-            // recursively happen until all the digits are added together
-            int sum = mod + digitalSum(divide);
-            // return the sum 
-            return sum;
-    }    
-    
-    public int digitalRoot(int n){
+        }
+        // if the number is not a single digit 
+        // create a mod integer to yield the rightmost digit
+        int mod = n % 10;
+        // create a divide integer to remove the rightmost digit
+        int divide = n / 10;
+        // create a sum integer that goes through mod and divide 
+        // recursively happen until all the digits are added together
+        int sum = mod + digitalSum(divide);
+        // return the sum 
+        return sum;
+    }
+
+    public int digitalRoot(int n) {
         // create an integer that stores the sum of the digits that
-        int sumOfDigits = digitalSum(n);
+        n = digitalSum(n);
         // if digitalSum is a single digit return that digit
-        if (sumOfDigits >= 0 && sumOfDigits <= 9) {
+        if (n >= 0 && n <= 9) {
             // return the digitalSum
-            return sumOfDigits;
-        } 
+            return n;
+        }
         // if the digitalSum is not a single digit
         // create a mod and divide integer to go through all the digits in the digitalSum
-        int mod2 = sumOfDigits % 10;
-        int divide = sumOfDigits / 10;
+        int mod = n % 10;
+        int divide = n / 10;
         // add the digits in the digitalSum together
-        int ans = mod2 + digitalRoot(divide);
+        int ans = mod + digitalRoot(divide);
         // return the root
         return ans;
+    }
+
+    public int triangle(int n) {
+        // if n is 0 or 1 than return that digit
+        if (n == 0 || n == 1) {
+            // return 0 or 1
+            return n;
+        }
+        // if n is not 0 or 1
+        // start at n and subtract one each time until it gets to 1
+        int ans = n + triangle(n - 1);
+        // return the answer of the triangle
+        return ans;
+    }
+
+    public void hailstone(int n) {
+        // output the next number in the sequence each time
+        System.out.println(n);
+        // if the number is 1, stop the sequence
+        if (n == 1) {
+            // stop the sequence
+            return;
+        }
+        // if n can be divided by 2 evenly
+        if (n % 2 == 0) {
+            // if n is an even number than divide by 2
+            hailstone(n / 2);
+        } // if its not an even number
+        else {
+            // equation for odd number to get the next number in the sequence
+            hailstone(3 * n + 1);
+        }
+    }
+
+    public void binaryConvert(int n) {
+        String binary = new String();
+        // output the next number in the sequence each time
+        System.out.println(n);
+
+        // if n can be divided by 2 evenly
+        if (n % 2 == 0) {
+            // if n is an even number than divide by 2
+            binary("0");
+        } // if its not an even number
+        else {
+            binary("1");
+        }
+        // if the number is 1, stop the sequence
+        if (n == 1) {
+            // stop the sequence
+            return binary;
+        }
+
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       A2Q1 test = new A2Q1();
-       int answer = test.digitalSum(1234567);
-        System.out.println(answer);
-        int ans2 = test.digitalRoot(1234567);
-        System.out.println(ans2);
+        A2Q1 test = new A2Q1();
+//        int answer = test.digitalSum(1234567);
+//        System.out.println(answer);
+//        int ans2 = test.digitalRoot(1234567);
+//        System.out.println(ans2);
+//        int ans3 = test.triangle(3);
+//        System.out.println(ans3);
+//        test.hailstone(5);
+        test.binaryConvert(156);
+
     }
 }
